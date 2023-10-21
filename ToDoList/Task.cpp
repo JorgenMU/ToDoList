45e4d23bd1c3091dce8d1663f8b75561757d52f9
Jørgen Moseid Ulven <jorgenulven@gmail.com>
2023-10-21 23:14:29 +0200

    Implement task loading and saving to file functionality
    #1

--- ToDoList/Task.cpp
+++ ToDoList/Task.cpp
@@ -5,12 +5,43 @@
 #include <fstream>
 #include <algorithm>
 
+// Function to save tasks to a file.
 void saveTaskToFile(const std::vector<Task>& tasks, const std::string& fileName)
 {
+	std::ofstream ostream(fileName);
+	ostream << tasks.size();
+
+	// Loop through the tasks in the vector and save them to the file.
+	for (const Task& task : tasks) {
+		std::string description = task.description;
+		std::replace(description.begin(), description.end(), ' ', '_');
+
+		ostream << '\n' << description << ' ' << task.done;
+	}
 
 }
-
+// Function to load tasks from a file.
 std::vector<Task> loadTaskFromFile(const std::string& fileName)
 {
-	return std::vector<Task>();
+	if (!std::filesystem::exists(fileName)) {
+		return std::vector<Task>();
+	}
+
+	std::vector<Task> tasks;
+	std::ifstream istream(fileName);
+
+	int n;
+	istream >> n;
+
+	// Loop through the tasks and load them from the file.
+	for (int i = 0; i < n; i++) {
+		std::string desciption;
+		bool done;
+
+		istream >> desciption >> done;
+		std::replace(desciption.begin(), desciption.end(), ' ', '_');
+		tasks.push_back(Task{ desciption, done });
+	}
+
+	return tasks;
 }
--- ToDoList/Task.h
+++ ToDoList/Task.h
@@ -2,11 +2,15 @@
 #include <string>
 #include <vector>
 
+// Define a structure "Task" to represent tasks with a description and a "done" flag.
 struct Task
 {
 	std::string description;
 	bool done;
 };
 
+// Function declaration for saving tasks to a file.
 void saveTaskToFile(const std::vector<Task>& tasks, const std::string& fileName);
+
+// Function declaration for loading tasks from a file.
 std::vector<Task> loadTaskFromFile(const std::string& fileName);
