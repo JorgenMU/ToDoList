7662e18034f3c6dd61786a5f0be528e7dd610cc2
Jørgen Moseid Ulven <jorgenulven@gmail.com>
2023-10-21 22:14:36 +0200

    Add project files.

---
+++ ToDoList/Task.cpp
@@ -0,0 +1,16 @@
+#include "Task.h"
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include <algorithm>
+
+void saveTaskToFile(const std::vector<Task>& tasks, const std::string& fileName)
+{
+
+}
+
+std::vector<Task> loadTaskFromFile(const std::string& fileName)
+{
+	return std::vector<Task>();
+}
