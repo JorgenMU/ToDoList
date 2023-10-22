3f26984dcca9077b732a1fa6f00768ac9ddf3291
Jørgen Moseid Ulven <jorgenulven@gmail.com>
2023-10-22 22:44:29 +0200

    Add headline and decleration of the cotrols
    #3

--- ToDoList/MainFrame.cpp
+++ ToDoList/MainFrame.cpp
@@ -7,5 +7,11 @@
 // Implementation of the MainFrame class constructor.
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title)
 {
+	wxFont headlinrFont(wxFontInfo(wxSize(0, 36)).Bold());
+	wxFont mainFont(wxFontInfo(wxSize(0, 24)));
 
+	panel = new wxPanel(this);
+	panel->SetFont(mainFont);
+
+	headlineText = new wxStaticText(panel, wxID_ANY, "To-Do List", wxPoint(0, 22), wxSize(800, -1), wxALIGN_CENTER_HORIZONTAL);
 }
--- ToDoList/MainFrame.h
+++ ToDoList/MainFrame.h
@@ -6,5 +6,13 @@ class MainFrame : public wxFrame
 {
 public:
 	MainFrame(const wxString& title);  // Constructor to create the main frame.
-};
+private:
+	void CreateControls();
 
+	wxPanel* panel;
+	wxStaticText* headlineText;
+	wxTextCtrl* inputField;
+	wxButton* addButton;
+	wxCheckListBox* checkListButton;
+	wxButton* clearButton;
+};
